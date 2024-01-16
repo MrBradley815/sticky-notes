371146f1ccce52ac327cf1571ae3f969c0e0429f
Brad <pb_815@yahoo.com>
2024-01-15 22:50:43 -0600

    fixed cancel note edit bug

--- src/renderer/note.js
+++ src/renderer/note.js
@@ -1,15 +1,15 @@
-const textarea = document.querySelector('#note-text');
-const editNoteBtn = document.querySelector('#edit-note');
-const saveNoteBtn = document.querySelector('#save-note');
-const cancelNoteBtn = document.querySelector('#cancel-note');
-const deleteNoteBtn = document.querySelector('#delete-note');
+const textarea = document.querySelector("#note-text");
+const editNoteBtn = document.querySelector("#edit-note");
+const saveNoteBtn = document.querySelector("#save-note");
+const cancelNoteBtn = document.querySelector("#cancel-note");
+const deleteNoteBtn = document.querySelector("#delete-note");
 
 let content;
 
-editNoteBtn.addEventListener('click', handleEditNote);
-saveNoteBtn.addEventListener('click', handleSaveNote);
-cancelNoteBtn.addEventListener('click', handleCancelNote);
-deleteNoteBtn.addEventListener('click', handleDeleteNote);
+editNoteBtn.addEventListener("click", handleEditNote);
+saveNoteBtn.addEventListener("click", handleSaveNote);
+cancelNoteBtn.addEventListener("click", handleCancelNote);
+deleteNoteBtn.addEventListener("click", handleDeleteNote);
 
 function handleEditNote() {
   content = textarea.textContent;
@@ -21,30 +21,30 @@ function handleSaveNote() {
 }
 
 function handleCancelNote() {
-  content = textarea.textContent;
+  textarea.value = content;
   toggleEditNote(false);
 }
 
 function toggleEditNote(on) {
   if (on) {
     textarea.disabled = false;
-    textarea.style.webkitAppRegion = 'no-drag';
+    textarea.style.webkitAppRegion = "no-drag";
     textarea.focus();
-    editNoteBtn.style.display = 'none';
-    saveNoteBtn.style.display = 'inline-block';
-    cancelNoteBtn.style.display = 'inline-block';
+    editNoteBtn.style.display = "none";
+    saveNoteBtn.style.display = "inline-block";
+    cancelNoteBtn.style.display = "inline-block";
   } else {
     textarea.disabled = true;
-    textarea.style.webkitAppRegion = 'drag';
+    textarea.style.webkitAppRegion = "drag";
     textarea.blur();
-    editNoteBtn.style.display = 'inline-block';
-    saveNoteBtn.style.display = 'none';
-    cancelNoteBtn.style.display = 'none';
+    editNoteBtn.style.display = "inline-block";
+    saveNoteBtn.style.display = "none";
+    cancelNoteBtn.style.display = "none";
   }
 }
 
 function handleDeleteNote() {
-  const ok = confirm('Are you sure you want to delete this sticky note?');
+  const ok = confirm("Are you sure you want to delete this sticky note?");
 
   if (ok) {
     window.electronAPI.deleteNote();
